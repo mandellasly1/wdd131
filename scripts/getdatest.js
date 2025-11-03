30bdfb66589848f71c71562942a2937b116ff552
mandellasly1 <nakpomah@byupathway.edu>
2025-11-03 22:02:58 +0100

    well done

--- scripts/getdatest.js
+++ scripts/getdatest.js
@@ -3,3 +3,8 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Dynamically set last modified date
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+
+const script = document.createElement('script');
+script.src = 'getdates.js';
+document.head.appendChild(script);
